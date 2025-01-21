84b4e7bd42b53b71ff73fbcdca951cc7b6207751
aitor-glitch-blde <bonacheaitor@gmail.com>
2025-01-21 15:35:07 +0100

    Add files via upload

---
+++ javascript/main.js
@@ -0,0 +1,58 @@
+// Variables principales
+let myImage = document.querySelector('.logo'); // Imagen con clase 'logo'.
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+// Manejo de imágenes
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if (mySrc.includes('firefox.webp')) {
+        myImage.setAttribute('src', './img/Batman.webp'); // Cambiar la imagen.
+    } else {
+        myImage.setAttribute('src', './img/firefox.webp');
+    }
+};
+
+// Mensaje de bienvenida personalizado
+if (!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    let storedName = localStorage.getItem('name');
+    myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
+}
+
+// Función para establecer el nombre de usuario
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    if (!myName) {
+        alert('Name cannot be empty or cancelled.');
+        return;
+    }
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+}
+
+// Cambiar usuario al hacer clic en el botón
+myButton.onclick = function() {
+    setUserName();
+};
+
+// Ejemplo de comparación corregida
+let helado = "chocolate";
+
+if (helado === "chocolate") {
+    alert("Yay, Amo el helado de chocolate!");
+} else {
+    alert("Awwww, pero el chocolate es mi favorito");
+}
+
+// Función de multiplicación
+function multiplica(num1, num2) {
+    return num1 * num2;
+}
+
+let res = multiplica(5, 8);
+alert("El resultado de multiplicar 5 * 8 es: " + res);
+
+      
+      
